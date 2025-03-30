e61403766eb96fbb42393831095f6b6dbaa54c10
anemvamsi4 <84965880+anemvamsi4@users.noreply.github.com>
2025-03-30 22:51:54 +0530

    structed text - pymupdf4llm

--- src/extraction/text_extraction.py
+++ src/extraction/text_extraction.py
@@ -4,8 +4,9 @@ import pymupdf4llm
 
 
 class PDFTextExtractor:
-    def __init__(self, file_path: str):
+    def __init__(self, file_path: str, output_dir: str):
         self.file_path = file_path
+        self.output_dir = output_dir
         self.structured_text = ""
 
     def extract_markdown(self) -> str:
@@ -16,7 +17,10 @@ class PDFTextExtractor:
     def save_markdown(self, output_path: str = None):
         """Save extracted Markdown text."""
         if output_path is None:
-            output_path = self.file_path.replace(".pdf", ".md")
+            filename = self.file_path.split('/')[-1].replace(".pdf", ".md")
+            output_path = self.output_dir + filename
+
+            pathlib.Path(self.output_dir).mkdir(parents=True, exist_ok=True)
 
         pathlib.Path(output_path).write_bytes(self.structured_text.encode())
         print(f"Markdown saved to {output_path}")
@@ -26,7 +30,10 @@ class PDFTextExtractor:
         sections = self.parse_markdown_to_json()
 
         if output_path is None:
-            output_path = self.file_path.replace(".pdf", ".json")
+            filename = self.file_path.split('/')[-1].replace(".pdf", ".json")
+            output_path = self.output_dir + filename
+
+            pathlib.Path(self.output_dir).mkdir(parents=True, exist_ok=True)
 
         with open(output_path, "w", encoding="utf-8") as f:
             json.dump(sections, f, indent=4, ensure_ascii=False)
@@ -59,7 +66,8 @@ if __name__ == "__main__":
     import sys
 
     pdf_path = sys.argv[1]
-    extractor = PDFTextExtractor(pdf_path)
+    output_dir = sys.argv[2]
+    extractor = PDFTextExtractor(pdf_path, output_dir)
     
     # Extract text
     markdown_text = extractor.extract_markdown()
