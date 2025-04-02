804ccf1629394597c2aaaf85efd0fcc85aea6733
anemvamsi4 <84965880+anemvamsi4@users.noreply.github.com>
2025-04-02 22:04:12 +0530

    added chat models

---
+++ src/llms/__init__.py
@@ -0,0 +1,26 @@
+from .anthropic import ChatAnthropic, anthropic_models
+from .deepseek import ChatDeepSeek, deepseek_models
+from .open_ai import ChatOpenAI, openai_models
+
+def get_llm(platform: str, model: str = "default", **args):
+    """
+    Returns the appropriate chat model based on the specified platform and model.
+
+    Args:
+        platform (str): The platform to use. Options are "openai", "anthropic", or "deepseek".
+        model (str): The model to use. If not specified, the default for the platform is used.
+        **args: Additional arguments to pass to the model.
+
+    Returns:
+        Chat Model instance.
+    """
+
+    if platform == "anthropic":
+        return ChatAnthropic(model = anthropic_models[model], **args)
+    
+    if platform == "deepseek":
+        return ChatDeepSeek(model = deepseek_models[model], **args)
+    
+    if platform == "openai":
+        return ChatOpenAI(model = openai_models[model], **args)
+    
---
+++ src/llms/deepseek.py
@@ -0,0 +1,33 @@
+from langchain_deepseek import ChatDeepSeek
+
+deepseek_models = {
+    "deepseek-v3" : "deepseek-chat",
+    "deepseek-r1" : "deepseek-reasoner",
+    "default"     : "deepseek-reasoner"
+}
+
+if __name__ == "__main__":
+    import os
+    import getpass
+
+
+    def _set_env(var: str):
+        if not os.environ.get(var):
+            os.environ[var] = getpass.getpass(f"{var}: ")
+
+
+    _set_env("DEEPSEEK_API_KEY")
+
+    llm = ChatDeepSeek(
+            model = deepseek_models["default"],
+            temperature=0,
+            max_tokens=1024
+        )
+    
+    user_input = ["Give me a code base structure for Comparative study of Losses for Semantic Segmentation on LITS dataset,"
+                "using UNet and DeepLab V3+ models. Also with the code base structure, mention all the classes and functions"
+                " that are required with their description, for each file of code base."]
+    
+    response = llm.invoke([{"role": "user",
+                 "content": user_input}])
+    print(response.content)
